6dd2323e13eca4ac79db83072fb034b9ce0d000e
Stefan Eng <stefaneng13@gmail.com>
2015-10-24 17:25:34 -0700

    Subscribe socket.io server to store

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import Server from 'socket.io';
 
-export default function startServer() {
-  const io = new Server();
-  io.attach(8090);
+export default function startServer(store) {
+  const io = new Server().attach(8090);
+
+  // Emit 'state' to socket.io when Store changes
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    // Feed action event from clients directly into store
+    // Should probably put authentication here
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
