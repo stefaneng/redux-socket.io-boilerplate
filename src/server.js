842c30e4db4d75cc321aa9333c1d8741acb0ca70
Stefan Eng <stefaneng13@gmail.com>
2015-10-24 18:09:08 -0700

    Use namespace for socket.io

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ import Server from 'socket.io';
 export default function startServer(store) {
   const io = new Server().attach(8090);
 
+  const entries = io.of('/entries');
   // Emit 'state' to socket.io when Store changes
   store.subscribe(
-    () => io.emit('state', store.getState().toJS())
+    () => entries.emit('state', store.getState().toJS())
   );
 
-  io.on('connection', (socket) => {
+  entries.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS());
     // Feed action event from clients directly into store
     // Should probably put authentication here
