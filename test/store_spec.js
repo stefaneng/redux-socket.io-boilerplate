ee4a4e985a4060b8674e02796d2fadceae5f6059
Stefan Eng <stefaneng13@gmail.com>
2015-10-24 17:06:49 -0700

    Redux store

---
+++ test/store_spec.js
@@ -0,0 +1,21 @@
+import { Map, fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import makeStore from '../src/store';
+
+describe('store', () => {
+  it('is a Redux store configured with the correct reducer', () => {
+    const store = makeStore();
+
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      entries: ['item-one', 'item-two']
+    });
+
+    expect(store.getState()).to.equal(fromJS({
+      entries: ['item-one', 'item-two']
+    }));
+  });
+});
