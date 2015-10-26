2e25f5f167daf842962e8bc5f295e5e9b8790056
Stefan Eng <stefaneng13@gmail.com>
2015-10-25 21:01:46 -0700

    Redux on client side

--- app/main.js
+++ app/main.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import io from 'socket.io-client';
+import reducer from './reducer';
+import {createStore} from 'redux';
+
+const store = createStore(reducer);
 
 const socket = io(`${location.protocol}//${location.hostname}:8090/entries`);
 socket.on('state', state => {
-  console.log('state', state);
+  store.dispatch({type: 'SET_STATE', state});
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
