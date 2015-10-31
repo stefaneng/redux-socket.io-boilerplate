74ac03e5d4ec2b7451a0324e82f28eb6b6c83bf5
Stefan Eng <stefaneng13@gmail.com>
2015-10-30 21:19:10 -0700

    ADD_ITEM

--- app/reducer.js
+++ app/reducer.js
@@ -1,15 +1,19 @@
-import {Map} from 'immutable';
+import {List, Map} from 'immutable';
 
 function setState(state, newState) {
   return state.merge(newState);
 }
 
-export default function(state = Map(), action) {
+function addItem(e, item) {
+  return state.items.push(item);
+}
+
+export default function(state = Map({items: List()}), action) {
   switch (action.type) {
   case 'SET_STATE':
     return setState(state, action.state);
   case 'ADD_ITEM':
-    return state;
+    return state.merge(Map({items: List([action.item])}));
   default:
     return state;
   }
