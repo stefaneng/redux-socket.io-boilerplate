e4959fb6e5a73ee2e96eef23dfbe0e896a6ba0f6
Stefan Eng <stefaneng13@gmail.com>
2015-11-15 15:03:15 -0800

    Todolist from redux docs working

--- app/main.js
+++ app/main.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App.js';
-import io from 'socket.io-client';
-import reducer from './reducer';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import ReactDOM from 'react-dom'
+import App from './containers/App.js'
+import io from 'socket.io-client'
+import reducer from './reducer'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import {SET_STATE} from '../redux/actions'
 
-const store = createStore(reducer);
+const store = createStore(reducer)
 
-const socket = io(`${location.protocol}//${location.hostname}:8090/entries`);
+const socket = io(`${location.protocol}//${location.hostname}:8090/entries`)
 socket.on('state', state => {
-  store.dispatch({type: 'SET_STATE', state});
+  store.dispatch({type: SET_STATE, state})
 });
 
 ReactDOM.render(
