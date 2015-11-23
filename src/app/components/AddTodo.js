93d4ccbc4e54cc25ff83681df81177b2716d495f
Stefan Eng <stefaneng13@gmail.com>
2015-11-22 16:46:25 -0800

    Enter key to submit

--- src/app/components/AddTodo.js
+++ src/app/components/AddTodo.js
@@ -1,18 +1,26 @@
 import React, { Component, PropTypes } from 'react'
 
+const ENTER_KEY = 13
+
 export default class AddTodo extends Component {
   render() {
     return (
       <div>
-        <input type='text' ref='input' />
-        <button onClick={(e) => this.handleClick(e)}>
+        <input type='text' ref='input' onKeyDown={(e) => this.checkKeyPress(e)} />
+        <button onClick={(e) => this.submitInput(e)}>
           Add
         </button>
       </div>
     )
   }
 
-  handleClick(e) {
+  checkKeyPress(e) {
+    if (e.keyCode === ENTER_KEY) {
+      this.submitInput(e)
+    }
+  }
+
+  submitInput(e) {
     const node = this.refs.input
     const text = node.value.trim()
     this.props.onAddClick(text)
